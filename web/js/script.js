4d7f7e2c524959d1e71efcdd34205d807b51afe3
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-08-24 21:51:56 +0800

    Feature: Character Sheet Data assembler for backend submission

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,54 @@
-const form = document.getElementById('form');
+const form = document.getElementById('CharacterSheetForm');
+const submit = document.getElementById('CharacterSheetSubmit');
 
-// const submit = document.getElementById('button');
-// submit.onclick = () =>
-// {
-//   console.log(form.name.value);
-// }
+submit.onclick = async event =>
+{
+  event.preventDefault();
+
+  /** This contains all the prefixes of all dynamic fields. */
+  const multipartPrefixes = [
+    'class',
+    'level',
+    'other_proficiency',
+    'language',
+    'atk_name',
+    'atk_bonus',
+    'atk_damage',
+    'feature_name',
+    'feature_description',
+    'cantrip',
+    'spell_one',
+    'spell_two',
+    'spell_three',
+    'spell_four',
+    'spell_five',
+    'spell_six',
+    'spell_seven',
+    'spell_eight',
+    'spell_nine'
+  ];
+
+  /** Initiallize each multi-part prefix as an array for the form data. */
+  let initialFormData = {}
+  for(const prefix of multipartPrefixes)
+    initialFormData[prefix] = []
+
+  /** Assemble form data to be submitted to backend. */
+  const formData = Object.values(form.elements).reduce((formData, { name, value }) =>
+  {
+    const [prefix, ...others] = multipartPrefixes.filter(item => name.startsWith(`${item}-`));
+    if(prefix)
+      formData[prefix].push(value);
+    else if(prefix == undefined)
+      formData[name] = value;
+    return formData;
+  }, initialFormData);
+
+  /* https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch */
+  /** Send data to backend. */
+  fetch('/character-sheet',
+  {
+    method: 'POST',
+    body: JSON.stringify(formData),
+  });
+}
